d157a4be732356faf86233b5f99f40ba8466b2bb
daveinthahood <davidesimone96@outlook.it>
2024-06-17 08:53:36 +0200

    ciao

---
+++ src/WelcomePage/welcome.jsx
@@ -0,0 +1,18 @@
+import './welcome.css'
+
+
+const Welcome = () => {
+    
+    
+    return (
+        <>
+            <div>
+                <h3> Welcome in Tranding Project </h3>
+                <button> Read More</button>
+            </div>
+
+        </>
+    )
+}
+
+export default Welcome
